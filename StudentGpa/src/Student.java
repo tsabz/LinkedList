844c99b57bf53615f0565d29676f629efb491016
Tonia <tonia.saba@gmail.com>
2022-01-08 11:30:19 -0500

    Student Inheritance

---
+++ StudentGpa/src/Student.java
@@ -0,0 +1,13 @@
+
+public class Student {
+	private String name;
+	private String address;
+	private Double GPA;
+	
+	// constructor method
+	public Student(String name, String address, Double GPA) {
+		this.name = name;
+		this.address = address;
+		this.GPA = GPA;
+	}
+}
---
+++ StudentGpa/src/StudentGpa.java
@@ -0,0 +1,9 @@
+
+public class StudentGpa {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
